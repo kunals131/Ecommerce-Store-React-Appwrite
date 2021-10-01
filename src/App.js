68e3b79521c3e517e9e49620c38e06edaf5fd43b
kunals131 <86314140+kunals131@users.noreply.github.com>
2021-10-01 14:37:45 +0530

    Added signin and sign up functionality

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import { Switch, Route } from 'react-router-dom'
 
 import Homepage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
-
+import Header from './components/header/header.component'
+import SignInAndSignUpPage from './pages/sign-in and sign-up/sign-in and sign-up.compoenent';
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
       <Route exact path = '/' component = {Homepage} />
       <Route exact path = '/shop' component = {ShopPage} />
+      <Route exact patht = '/signin' component= {SignInAndSignUpPage}></Route>
       </Switch>
     </div>
   );
